d17037b7b646438912560cc233568d14c12ff0bc
Ayuan <76616749+Ayuan2805@users.noreply.github.com>
2023-03-29 12:12:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ "$(id -u)" = 0 ]; then
-    echo "##################################################################"
-    echo "This script MUST NOT be run as root user since it can arouse unpredicted"
-    echo "problems and you may have to reinstall arch. So run this script as a"
-    echo "normal user. You will be asked for a sudo password when necessary"
-    echo "##################################################################"
+    echo "######################################################################"
+    echo "This script should NOT be run as root user as it may create unexpected"
+    echo " problems and you may have to reinstall Arch. So run this script as a"
+    echo "  normal user. You will be asked for a sudo password when necessary"
+    echo "######################################################################"
     exit 1
 fi
 
