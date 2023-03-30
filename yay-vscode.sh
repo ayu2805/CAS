bab914f103e91a7c66a816730fb53e53e8dbc00a
Ayuan <76616749+Ayuan2805@users.noreply.github.com>
2023-03-30 18:56:14 +0530

    Create yay-vscode.sh

---
+++ yay-vscode.sh
@@ -0,0 +1,10 @@
+read -r -p "Do you want to install yay? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+fi
+read -r -p "Do you want to install VS Code(from AUR)? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    yay -Syu visual-studio-code-bin
+fi
