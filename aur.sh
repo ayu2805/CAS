5960f5803f41766a83d83fc4fdc0a5cd95a81a8c
Ayuan <76616749+Ayuan2805@users.noreply.github.com>
2023-03-31 11:21:51 +0530

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -6,7 +6,13 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     cd yay
     makepkg -si
 fi
+
 read -r -p "Do you want to install VS Code(from AUR)? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     yay -Syu visual-studio-code-bin
+fi
+
+read -r -p "Do you want to install VS Code(from AUR)? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    yay -Syu onlyoffice-bin
 fi
