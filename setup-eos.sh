d0aef12eaa73521d34f76af58938215e41c41c56
Ayuan2805 <ayushmaanpadhi2805@gmail.com>
2023-05-01 12:04:12 +0530

    Update

---
+++ setup-eos.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ "$(id -u)" = 0 ]; then
+    echo "######################################################################"
+    echo "This script should NOT be run as root user as it may create unexpected"
+    echo " problems and you may have to reinstall Arch. So run this script as a"
+    echo "  normal user. You will be asked for a sudo password when necessary"
+    echo "######################################################################"
+    exit 1
+fi
+
+echo "################################"
+echo "## Install & Update packages. ##"
+echo "################################"
+
+sudo pacman -Syu --needed - < ttpkg
+echo "QT_QPA_PLATFORMTHEME=qt6ct" | sudo tee /etc/environment
+
+yay -Syu systemd-numlockontty
+sudo systemctl enable numLockOnTty
+
+read -r -p "Do you want to install xfce? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    yay -Syu --needed - < xfce
+fi
+
+read -r -p "Do you want to install cinnamon? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    yay -Syu --needed - < cinnamon
+    cp /etc/X11/xinit/xinitrc ~/.xinitrc
+    echo "exec cinnamon-session" | tee -a ~/.xinitrc
+fi
+
+echo "#########################"
+echo "## Install themes. ##"
+echo "#########################"
+
+yay -Syu kvantum-theme-materia materia-gtk-theme
+git clone https://github.com/vinceliuice/Fluent-icon-theme.git
+cd Fluent-icon-theme
+sudo ./install.sh -r
+
+echo "################################################"
+echo "Done! You can manually delete this folder later!"
+echo "############# Reboot is recomended #############"
+echo "################################################"
+
+read -r -p "Do you want to reboot now? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    reboot
+fi
