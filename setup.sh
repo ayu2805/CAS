acff713e8e255e96a91a1e537e9ea3f07cfc783e
Ayuan2805 <ayushmaanpadhi2805@gmail.com>
2023-05-01 18:26:08 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -24,14 +24,9 @@ cd ..
 yay -Syu --needed systemd-numlockontty
 sudo systemctl enable numLockOnTty
 
-read -r -p "Do you want to install xfce? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    sudo pacman -Syu - < xfce
-fi
-
 read -r -p "Do you want to install cinnamon? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    sudo pacman -Syu - < cin
+    sudo pacman -Syu - < cinxfce
     yay -S mint-artwork
     sudo sed -i '/^#greeter-session=/s/^#greeter-session=example-gtk-gnome/greeter-session=lightdm-slick-greeter/' /etc/lightdm/lightdm.conf
     sudo systemctl enable lightdm
