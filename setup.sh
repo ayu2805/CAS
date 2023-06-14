2494c158e23ba0cce3f3216e33019de338304907
Ayu <76616749+ayu2805@users.noreply.github.com>
2023-06-14 22:19:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,11 +39,11 @@ if ! sudo grep -qF "$line" "$file"; then
     echo "$line" | sudo tee -a "$file" > /dev/null
 fi
 
-echo "Installing Cinnamon..."
-sudo pacman -Syu --needed --noconfirm - < cin
-gsettings set org.cinnamon.desktop.interface gtk-theme "Materia-dark-compact" && gsettings set org.cinnamon.theme name "Materia-dark-compact" && gsettings set org.cinnamon.desktop.interface icon-theme "Fluent-dark"
+#echo "Installing Cinnamon..."
+#sudo pacman -Syu --needed --noconfirm - < cin
+#gsettings set org.cinnamon.desktop.interface gtk-theme "Materia-dark-compact" && gsettings set org.cinnamon.theme name "Materia-dark-compact" && gsettings set org.cinnamon.desktop.interface icon-theme "Fluent-dark"
 sudo sed -i 's/^#greeter-setup-script=/greeter-setup-script=\/usr\/bin\/numlockx\ on/' /etc/lightdm/lightdm.conf
-sudo systemctl enable lightdm
+#sudo systemctl enable lightdm
 echo -e "[greeter]\ntheme-name = Materia-dark-compact\nicon-theme-name = Fluent-dark\nhide-user-image = true\nindicators = ~clock;~spacer;~session;~a11y;~power" | sudo tee /etc/lightdm/lightdm-gtk-greeter.conf > /dev/null
 
 #sudo pacman -Syu --needed --noconfirm - < g
