5da8fb228c664fd186341162ebdb05d6b9d5ee04
Ayu <76616749+ayu2805@users.noreply.github.com>
2023-06-16 22:37:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,6 +59,8 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     xfconf-query -c xfwm4 -p /general/theme -s "Materia-dark-compact"
 fi
 
+sudo pacman -Rscn xdg-desktop-portal-gnome
+
 sudo systemctl enable lightdm
 sudo sed -i 's/^#greeter-setup-script=/greeter-setup-script=\/usr\/bin\/numlockx\ on/' /etc/lightdm/lightdm.conf
 echo -e "[greeter]\ntheme-name = Materia-dark-compact\nicon-theme-name = Fluent-dark\nhide-user-image = true\nindicators = ~clock;~spacer;~session;~a11y;~power" | sudo tee /etc/lightdm/lightdm-gtk-greeter.conf > /dev/null
